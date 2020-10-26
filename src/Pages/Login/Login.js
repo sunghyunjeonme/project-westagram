4a01ee719278298f2a192281597a15e83c14fb97
hyuns619 <4dallove@gmail.com>
2020-10-26 19:26:09 +0900

    Add : id/pw 이벤트 핸들링 및 validation 입력한 id/pw 조건이 맞을 때 button 색상 활성화 로그인 버튼 클릭 시 main 페이지로 이동

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,7 +1,42 @@
+/* eslint-disable no-unused-vars */
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 import "./Login.scss";
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: "",
+      password: "",
+    };
+  }
+
+  checkValidity = (e) => {
+    const { id, password } = this.state;
+    const idValidity = id.includes("@");
+    const pwValidity = password.length >= 5;
+    if (idValidity && pwValidity) {
+      this.props.history.push("/main");
+    }
+    if (!idValidity) {
+      alert("아이디는 @를 포함해야 합니다.");
+    }
+    if (!pwValidity) {
+      alert("비밀번호는 영문 5글자 이상이어야 합니다.");
+    }
+  };
+
+  handleIdChange = (e) => {
+    console.log();
+    this.setState({ id: e.target.value });
+  };
+
+  handlePwChange = (e) => {
+    console.log(e.target.value);
+    this.setState({ password: e.target.value });
+  };
+
   goToFacebook = () => {
     window.open("http://facebook.com");
   };
@@ -34,16 +69,30 @@ class Login extends Component {
               </header>
               <form className="login-form">
                 <input
+                  onChange={this.handleIdChange}
                   className="input-login-id"
                   type="text"
                   placeholder="전화번호, 사용자 이름 또는 이메일"
                 />
                 <input
+                  onChange={this.handlePwChange}
                   className="input-login-pw"
                   type="password"
                   placeholder="비밀번호"
                 />
-                <button className="login-btn">로그인</button>
+                <button
+                  className="login-btn"
+                  onClick={this.checkValidity}
+                  style={{
+                    backgroundColor:
+                      this.state.id.includes("@") &&
+                      this.state.password.length >= 5
+                        ? "#2895F6"
+                        : "rgba(0, 149, 246, .3)",
+                  }}
+                >
+                  로그인
+                </button>
                 <div className="line-or-line-wrapper">
                   <div className="line"></div>
                   <div className="or">또는</div>
